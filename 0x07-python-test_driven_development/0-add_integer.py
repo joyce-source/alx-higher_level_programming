9ddea31ae79c60c85d976202d593224361490df9
joyce-source <jojobrown97@gmail.com>
2023-04-08 14:02:31 +0000

    test_dev

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+# 0-add_integer.py
+"""Defines an integer addition function."""
 
-def matrix_divided(matrix, div):
-# Check if matrix is a list of lists of integers/floats
-if not all(isinstance(row, list) and all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
-raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-# Check if all rows have the same size
-if not all(len(row) == len(matrix[0]) for row in matrix):
-raise TypeError("Each row of the matrix must have the same size")
-# Check if div is a number
-if not isinstance(div, (int, float)):
-raise TypeError("div must be a number")
-# Check if div is not zero
-if div == 0:
-raise ZeroDivisionError("division by zero")
-# Divide all elements of the matrix by div, rounded to 2 decimal places
-new_matrix = [[round(elem/div, 2) for elem in row] for row in matrix]
-return new_matrix
+
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
