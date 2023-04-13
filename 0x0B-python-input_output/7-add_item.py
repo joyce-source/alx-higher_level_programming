85f3857b3457a1867141f27e75c25c7059bf66d5
joyce-source <brownjojo97@gmail.com>
2023-04-13 06:46:54 -0700

    tsk7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,69 @@
+#!/usr/bim/python3
+
+"""
+script adds all arg to Python list and saves them to file in JSON format.
+
+Functions:
+    save_to_json_file(my_obj, filename):
+        Saves a Python object to a file in JSON format.
+        Args:
+            my_obj: The Python object to be saved to the file.
+            filename: The name of the file to be saved.
+        Returns:
+            None.
+
+    load_from_json_file(filename):
+        Loads a Python object from a file in JSON format.
+        Args:
+            filename: The name of the file to load the object from.
+        Returns:
+            The Python object loaded from the file.
+"""
+
+import json
+import sys
+
+from typing import List
+
+
+def save_to_json_file(my_obj: List[str], filename: str) -> None:
+    """
+    Saves a Python object to a file in JSON format.
+
+    Args:
+        my_obj: The Python object to be saved to the file.
+        filename: The name of the file to be saved.
+
+    Returns:
+        None.
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
+
+
+def load_from_json_file(filename: str) -> List[str]:
+    """
+    Loads a Python object from a file in JSON format.
+
+    Args:
+        filename: The name of the file to load the object from.
+
+    Returns:
+        The Python object loaded from the file.
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+if __name__ == '__main__':
+    # Add all arguments to a Python list.
+    arguments = sys.argv[1:]
+    # Load the list from the file, if it exists.
+    try:
+        saved_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        saved_list = []
+    # Add the new arguments to the list.
+    saved_list += arguments
+    # Save the updated list to the file.
+    save_to_json_file(saved_list, 'add_item.json')
