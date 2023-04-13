83ecfd322a787e827555f675e77e0ee728bf6973
joyce-source <brownjojo97@gmail.com>
2023-04-13 06:42:10 -0700

    tsk6_error

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,19 +7,20 @@ This module provides functions to handle JSON data
 
 import json
 
-"""
-load_from_json_file(filename)
-    Load an object from a JSON file.
+
+def load_from_json_file(filename):
+
+    """
+        Load an object from a JSON file.
 
     Args:
         filename: A string representing the path to the JSON file.
 
     Returns:
-        The Python object represented by the JSON file
+        The Python object represented by the JSON filename
 """
 
 
-def load_from_json_file(filename):
-    with open(filename, "r") as f:
-        obj = json.load(f)
+with open(filename, "r") as f:
+    obj = json.load(f)
     return obj
