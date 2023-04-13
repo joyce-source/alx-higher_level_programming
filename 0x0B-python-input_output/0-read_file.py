06d803bfa749831992db3bf95274c939ba71b07a
joyce-source <jojobrown97@gmail.com>
2023-04-13 11:20:17 +0000

    task_debug

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+"""
+This module provides a function for reading a text file and printing its contents to the standard output.
+"""
+
 def read_file(filename=""):
+    """
+    Reads the contents of a text file and prints them to the standard output.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to "".
+
+    Returns:
+        None
+    """
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content)
