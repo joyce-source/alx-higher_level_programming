8c8eba7b59239cc489d7472a539ce1629db305f8
joyce-source <jojobrown97@gmail.com>
2023-04-13 11:41:34 +0000

    task4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
 
+"""
+This module contains a function for serializing Python objects to JSON.
+
+The `json` module is used for serialization, so the function assumes that the
+input object can be serialized as JSON.
+
+Example Usage:
+--------------
+my_dict = {"key": "value", "num": 42}
+json_string = to_json_string(my_dict)
+
+# Output:
+# '{"key": "value", "num": 42}'
+"""
+
+
 import json
 
 def to_json_string(my_obj):
     """
-    Returns the JSON representation of an object as a string.
+    Serialize a Python object to JSON format.
 
     Args:
         my_obj: The object to be serialized.
 
     Returns:
-        str: The JSON representation of the object as a string.
-
-    Raises:
-        None.
+        str: The JSON representation of the input object as a string.
     """
     return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,29 @@
+"""
+This module contains a function for deserializing a JSON string into a Python object.
+
+The `json` module is used for deserialization, so the function assumes that the input
+string is a valid JSON representation of a Python object.
+
+Example Usage:
+--------------
+json_string = '{"key": "value", "num": 42}'
+my_dict = from_json_string(json_string)
+
+# Output:
+# {'key': 'value', 'num': 42}
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    Deserialize a JSON string into a Python object.
+
+    Args:
+        my_str (str): The JSON string to be deserialized.
+
+    Returns:
+        object: The Python object represented by the input JSON string.
+    """
+    return json.loads(my_str)
