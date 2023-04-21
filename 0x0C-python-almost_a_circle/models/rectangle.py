90e336de5119b1f6e58e7066e6368c11412d3a02
joyce-source <brownjojo97@gmail.com>
2023-04-21 06:34:58 -0700

    task_rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,111 +1,55 @@
 #!/usr/bin/python3
+''' First rectangle
+'''
 
 from models.base import Base
 
+
 class Rectangle(Base):
-    """
-    Represents a rectangle.
-
-    Attributes:
-    - id (int): The id of the rectangle.
-    - width (int): The width of the rectangle.
-    - height (int): The height of the rectangle.
-    - x (int): The x-coordinate of the rectangle.
-    - y (int): The y-coordinate of the rectangle.
-
-    Methods:
-    - __init__(self, width, height, x=0, y=0, id=None): Initializes a new instance of the Rectangle class.
-    - width(self): Gets the width of the rectangle.
-    - width(self, value): Sets the width of the rectangle to the specified value.
-    - height(self): Gets the height of the rectangle.
-    - height(self, value): Sets the height of the rectangle to the specified value.
-    - x(self): Gets the x-coordinate of the rectangle.
-    - x(self, value): Sets the x-coordinate of the rectangle to the specified value.
-    - y(self): Gets the y-coordinate of the rectangle.
-    - y(self, value): Sets the y-coordinate of the rectangle to the specified value.
-    """
+    '''class Rectangle inherits from base
+    '''
+    KV_dict = {'id': 'id', 'width': '_Rectangle__width',
+               'height': '_Rectangle__height',
+               'x': '_Rectangle__x', 'y': '_Rectangle__y'}
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initializes a new instance of the Rectangle class.
-
-        Args:
-        - width (int): The width of the rectangle.
-        - height (int): The height of the rectangle.
-        - x (int): The x-coordinate of the rectangle (default: 0).
-        - y (int): The y-coordinate of the rectangle (default: 0).
-        - id (int): The id of the rectangle (default: None).
-        """
+        '''method __init__ Initialization a Rectangle
+        '''
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-    @property
-    def width(self):
-        """
-        Gets the width of the rectangle.
-        """
-        return self.__width
+    def update(self, *args, **kwargs):
+        '''method update
+        '''
+        key_list = ['id', '_Rectangle__width', '_Rectangle__height',
+                    '_Rectangle__x', '_Rectangle__y']
+        KV_dict = {'id': 'id', 'width': '_Rectangle__width',
+                   'height': '_Rectangle__height',
+                   'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+        for idx, el in enumerate(args):
+            self.__dict__[key_list[idx]] = el
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__dict__[KV_dict[key]] = val
 
-    @width.setter
-    def width(self, value):
-        """
-        Sets the width of the rectangle to the specified value.
+    def __str__(self):
+        '''method __str__
+        '''
 
-        Args:
-        - value (int): The new width of the rectangle.
-        """
-        self.__width = value
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                self.__y, self.__width, self.__height))
 
-    @property
-    def height(self):
-        """
-        Gets the height of the rectangle.
-        """
-        return self.__height
+    def area(self):
+        '''public_method area of rectangle
+        '''
+        return self.__width * self.__height
 
-    @height.setter
-    def height(self, value):
-        """
-        Sets the height of the rectangle to the specified value.
-
-        Args:
-        - value (int): The new height of the rectangle.
-        """
-        self.__height = value
-
-    @property
-    def x(self):
-        """
-        Gets the x-coordinate of the rectangle.
-        """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """
-        Sets the x-coordinate of the rectangle to the specified value.
-
-        Args:
-        - value (int): The new x-coordinate of the rectangle.
-        """
-        self.__x = value
-
-    @property
-    def y(self):
-        """
-        Gets the y-coordinate of the rectangle.
-        """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """
-        Sets the y-coordinate of the rectangle to the specified value.
-
-        Args:
-        - value (int): The new y-coordinate of the rectangle.
-        """
-        self.__y = value
+    def display(self):
+        '''public method display self prints in stdout #
+        '''
+        print("\n" * (self.__y), end="")
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
