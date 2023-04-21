0b5a0e01e030e9ac0a9987d5ac68ef27dccddb94
joyce-source <brownjojo97@gmail.com>
2023-04-21 06:29:05 -0700

    task_base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,66 @@
 #!/usr/bin/python3
-# Create a folder named models with an empty file __init__.py inside
-# This file makes the folder a Python package
+''' models/base.py
+'''
+import json
+
 
 class Base:
-    # private class attribute __nb_objects = 0
+    ''' first class Base
+    '''
+
     __nb_objects = 0
-    
-    # class constructor: def __init__(self, id=None)
+
     def __init__(self, id=None):
-        # if id is not None, assign the public instance attribute id with this argument value
-        # otherwise, increment __nb_objects and assign the new value to the public instance attribute id
+        ''' initialize Base  __init__
+        '''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''method: save_to_file
+        '''
+        list_dicts = []
+        if list_objs is not None:
+            for obj in list_objs:
+                tmp_dict = cls.to_dictionary(obj)
+                list_dicts.append(tmp_dict)
+            json_L_of_D = cls.to_json_string(list_dicts)
+        else:
+            json_L_of_D = "[]"
+
+        filename = cls.__name__ + ".json"
+
+        with open(filename, "w") as file:
+            num_char = file.write(json_L_of_D)
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' method json string
+        '''
+        ret_list = []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        if type(list_dictionaries) != list:
+            ret_list.append(list_dictionaries)
+        else:
+            ret_list = list_dictionaries
+        return json.dumps(ret_list)
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' method json string
+        '''
+        ret_list = []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        if type(list_dictionaries) != list:
+            ret_list.append(list_dictionaries)
+        else:
+            ret_list = list_dictionaries
+        return json.dumps(ret_list)
