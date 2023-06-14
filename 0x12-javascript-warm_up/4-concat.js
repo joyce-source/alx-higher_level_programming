894538257b733e219088de9f71fcf70cde0bd19d
joyce-source <brownjojo@gmail.com>
2023-06-14 01:35:42 -0700

    4debug

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const [arg1, arg2] = process.argv.slice(2);
 
-if (arg1 !== undefined && arg2 !== undefined) {
-  console.log(`${arg1} is ${arg2}`);
-} else {
-  console.log('Please provide two arguments.');
-}
+console.log(arg1 + ' is ' + arg2);
