17003c00081b6e9bc6ac017a93cb4cc5fefc8f32
joyce-source <brownjojo@gmail.com>
2023-06-14 01:51:43 -0700

    debug10_13

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
-function add (a, b) {
-  return a + b;
-}
-
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-const num1 = parseInt(arg1);
-const num2 = parseInt(arg2);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  const result = add(num1, num2);
+function add(a, b) {
+  const result = a + b;
   console.log(result);
-} else {
-  console.log('Invalid input. Please provide two integers.');
 }
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+add(arg1, arg2);
