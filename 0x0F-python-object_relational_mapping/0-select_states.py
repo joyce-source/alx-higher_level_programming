9b6923da119683523875723b29b02be9329632d3
joyce-source <brownjojo@gmail.com>
2023-06-21 01:02:48 -0700

    obj_mapping_debug_v

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,31 +16,31 @@ def list_states(username, password, database):
     Returns:
         None
 """
-    # Connect to the MySQL server
+    """Connect to the MySQL server"""
     db = MySQLdb.connect(user=username,
                          passwd=password,
                          db=database,
                          host='localhost',
                          port=3306)
 
-    # Create a cursor object to execute SQL queries
+    """Create a cursor object to execute SQL queries"""
     cursor = db.cursor()
 
-    # Execute the SQL query to fetch all states
+    """Execute the SQL query to fetch all states"""
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows returned by the query
+    """Fetch all the rows returned by the query"""
     rows = cursor.fetchall()
 
-    # Print the results
+    """Print the results"""
     for row in rows:
         print(row)
 
-    # Close the cursor and database connection
+   """Close the cursor and database connection"""
     cursor.close()
     db.close()
 
-# Run the script with the provided arguments
+""" Run the script with the provided arguments"""
 
 
 if __name__ == "__main__":
