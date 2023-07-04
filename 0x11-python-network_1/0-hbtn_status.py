205988d3681c6dad25d11f238d4f7fc70bbe7479
joyce-source <brownjojo96@gmail.com>
2023-07-04 01:42:24 -0700

    status_0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status using urllib.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+    print("- Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(utf8_content))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+# Python - Network #1
+
+This project is a Python script that fetches the status of a url. It demonstrates how to make an HTTP request and retrieve the response body.
