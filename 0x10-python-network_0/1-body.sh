30216d6aadc1bb05ebb263355e8ff4f0f69a036b
joyce-source <brownjojo96@gmail.com>
2023-07-04 00:29:02 -0700

    debbug_1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a GET request to URL and displays body of response (if code is 200)
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-[[ "$response" == "200" ]] && curl -s "$1" || echo "Unexpected response with status code: $response"
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1" | sed '1,/^$/d'
