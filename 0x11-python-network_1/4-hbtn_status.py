c2a0bb1248967539c0aad12abf4ad83115da6dde
joyce-source <brownjojo96@gmail.com>
2023-07-04 02:44:23 -0700

    mystatus

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,13 +2,13 @@
 """
 This script uses the requests package to fetch a URL and display the body of the response.
 """
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
 
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
+    response = requests.get(url)
+    body = response.text
 
     print("Body response:")
     print("    - type: {}".format(type(body)))
