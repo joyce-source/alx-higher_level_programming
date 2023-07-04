bf2f706f26b5e68bdd3d2bbcd32d73e317b38f61
joyce-source <brownjojo96@gmail.com>
2023-07-04 00:48:24 -0700

    debbug_4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL with custom header and displays body of response
-
-URL=$1
-curl -s -H "X-School-User-Id: 98" "$URL"
-
+curl -s -H "X-School-User-Id: 98" "$1"
