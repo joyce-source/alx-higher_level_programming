073c98bd91dc2fa9b110c888b84a72a41767efb3
joyce-source <brownjojo96@gmail.com>
2023-07-04 00:23:54 -0700

    debbug_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to URL and displays the size of the response body in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print "Response size: " $2 " bytes"}'
+curl -s "$1" | grep -oP 'GET \/ => "\K[^"]+'
