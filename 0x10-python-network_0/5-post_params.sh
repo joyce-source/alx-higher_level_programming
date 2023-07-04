47598c8c5fcfd3a49cfa489c38cf0ffa8afbd8ea
joyce-source <brownjojo96@gmail.com>
2023-07-04 00:51:41 -0700

    debbug_5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# This script sends a POST request to a URL with variables in the request body and
-# displays the response body
-
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-
-curl -s -X POST -d "email=$EMAIL&subject=$SUBJECT" "$URL"
-
+# This script sends a POST request to a URL with variables in the request body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
