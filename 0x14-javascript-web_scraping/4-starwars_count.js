b7ec160df9122173ae3dd3f78bde3885b462dc92
joyce-source <brownjojo96@gmail.com>
2023-07-26 12:13:33 +0000

    store_count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmsData = JSON.parse(body);
+    const moviesWithWedgeAntilles = filmsData.results.filter((movie) =>
+      movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+    );
+    console.log(moviesWithWedgeAntilles.length);
+  }
+});
